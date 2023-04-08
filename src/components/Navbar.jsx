72bfd4af044e2fcb85bbe867dbac90abdea934ba
rshelar21 <89639649+rshelar21@users.noreply.github.com>
2023-04-09 03:12:28 +0530

    search-page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const [scrolled, setScrolled] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY >= 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        });
+
+        return () => {
+            window.removeEventListener("scroll", () => {});
+        };
+    }, [])
+
+  return (
+    <>
+      <Container scrolled={scrolled}>
+        <Left >
+          <Link to="/">LogoMaker AI</Link>
+        </Left>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 999;
+  position: fixed;
+  padding: 10px 50px;
+  background-color: ${(props) => (props.scrolled ? "#505ABC" : "transparent")};
+`;
+
+const Left = styled.div`
+  a {
+    color: #fff;
+    font-size: 30px;
+    font-weight: 600;
+  }
+`;
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+        <Main>
+          <h1>Business Name Generator</h1>
+          <h3>
+            generate a short, brandable business name using artificial
+            intelligence
+          </h3>
+
+          <InputField>
+            <input type="text" placeholder="Enter your business name" />
+            <Button>Generate</Button>
+          </InputField>
+        </Main>
+      </Container>
+
+      <About>
+        <Description>
+          <h1>Why a branded name?</h1>
+          <p>
+            For new businesses, naming options can seem quite limited. Short
+            domains are very expensive, yet longer multi-word names don’t
+            inspire confidence.
+          </p>
+          <p>
+            In 2023 many startups are choosing a short, branded name - a name
+            that’s unique, memorable and affordable.
+          </p>
+        </Description>
+      </About>
+    </>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url("/assets/background.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-color: #505abc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  color: #fff;
+  text-align: center;
+  h1 {
+    font-size: 45px;
+  }
+  h3 {
+    color: #bbd3f9;
+    font-size: 20px;
+  }
+`;
+
+const InputField = styled.div`
+  margin: 0 auto;
+  margin-top: 25px;
+  padding: 10px;
+
+  input {
+    border: none;
+    width: 400px;
+    padding: 15px 20px;
+    border-radius: 10px;
+    outline: none;
+    border: 1px solid #000;
+    font-size: 18px;
+    color: rgba(0, 0, 0, 0.5);
+
+    &::placeholder {
+      font-size: 18px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  background-color: #363a8e;
+  outline: none;
+  border: none;
+  color: #fff;
+  padding: 15px 20px;
+  border-radius: 10px;
+  margin-left: 10px;
+  width: 130px;
+  transition: all 0.3s ease;
+  font-weight: 500;
+  font-size: 16px;
+  letter-spacing: 1px;
+
+  &:active {
+    scale: 1.05;
+  }
+`;
+
+const About = styled.div`
+  width: 100%;
+  background-image: url("/assets/background2.svg");
+  height: 400px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: #d1e1ed;
+  background-size: cover;
+`;
+
+const Description = styled.div`
+  width: 100%;
+  text-align: center;
+  padding-top: 50px;
+  max-width: 400px;
+  margin: 0 auto;
+
+  h1 {
+    color: #5874db;
+    font-size: 40px;
+  }
+  p {
+    color: #687689;
+    font-size: 18px;
+  }
+`;
+
+export default Home;
